8d033407eb3834b0c001b117588e3200aff43765
Leon Katz <lgoldner@ringmountain.org>
2019-07-28 21:25:40 -0700

    Breadth search works too

--- main.cpp
+++ main.cpp
@@ -5,8 +5,15 @@ int main(int argc, char const *argv[]) {
 
   Graph g(10);
   g.AddEdge(0, 2);
-  g.AddEdge(0, 5);
+  g.AddEdge(2, 5);
+  g.AddEdge(5, 7);
+  g.AddEdge(7, 6);
   g.AddEdge(5, 4);
+  g.AddEdge(4, 3);
+  g.AddEdge(3, 7);
+
+  g.BreadthSearch(0);
+  cout << endl;
   g.DepthSearch(0);
   cout << endl;
   return 0;
